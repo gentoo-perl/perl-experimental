ab4f94737364e5a82928459a5e96c6416448dcb1
Kent Fredric <kentfredric@gmail.com>
2011-11-14 15:52:24 +1300

    Add support for --debug to gen_ebuild

--- scripts/gen_ebuild.pl
+++ scripts/gen_ebuild.pl
@@ -47,16 +47,14 @@ EOF
 }
 my ($release) = shift(@ARGV);
 
-
-
 *STDOUT->binmode(':utf8');
 *STDERR->binmode(':utf8');
 
 require deptools;
 
-my ( $release_info ) = deptools::get_deps( $release );
+my ($release_info) = deptools::get_deps($release);
 
-if ( not $release_info ){
+if ( not $release_info ) {
   die "Cannot find $release on MetaCPAN";
 }
 my $dep_phases = deptools::get_dep_phases($release);
@@ -71,119 +69,123 @@ for my $module ( keys %{ $dep_phases->{modules} } ) {
 my @squeue =
   sort { $a->[1]->[2] cmp $b->[1]->[2] or $a->[1]->[3] cmp $b->[1]->[3] or $a->[0] cmp $b->[0] } @queue;
 
-
 require dep::handler::stdout;
 require dep::handler::bashcode;
 
 my $handler = dep::handler::stdout->new();
-my $handler2 = dep::handler::bashcode->new();
-
+my $handler2 = dep::handler::bashcode->new( ( $flags->{debug} ? ( debug => 1 ) : () ), debug_handler => $handler, );
 
 for my $qi (@squeue) {
   deptools::dispatch_dependency_handler( $release, @{$qi}, $handler2 );
 }
 
-my $depends = [];
+my $depends  = [];
 my $rdepends = [];
 require POSIX;
-my $year = POSIX::strftime('%Y', gmtime);
+my $year = POSIX::strftime( '%Y', gmtime );
 
-my $path = deptools::gentooize_pkg($release_info->{distribution} );
+my $path = deptools::gentooize_pkg( $release_info->{distribution} );
 require Gentoo::PerlMod::Version;
-my $version = Gentoo::PerlMod::Version::gentooize_version( $release_info->{version} , { lax => 1 } );
+my $version = Gentoo::PerlMod::Version::gentooize_version( $release_info->{version}, { lax => 1 } );
 $env->root->subdir($path)->mkpath;
-my $file = $env->root->subdir($path)->file($release_info->{distribution} . '-' . $version . '.ebuild' );
+my $file = $env->root->subdir($path)->file( $release_info->{distribution} . '-' . $version . '.ebuild' );
 
-my ( $fh ) = $file->openw;
+my ($fh) = $file->openw;
 say "Writing $file";
 $fh->say("# Copyright 1999-$year Gentoo Foundation");
 $fh->say("# Distributed under the terms of the GNU General Public License v2");
 $fh->say("# \$Header: \$");
 $fh->say("EAPI=4");
-$fh->say("MODULE_AUTHOR=" . $release_info->{author});
-$fh->say("MODULE_VERSION=" . $release_info->{version});
+$fh->say( "MODULE_AUTHOR=" . $release_info->{author} );
+$fh->say( "MODULE_VERSION=" . $release_info->{version} );
 $fh->say('inherit perl-module');
 $fh->say('');
-if ( not defined $release_info->{abstract} ) { 
-  $fh->say('DESCRIPTION=\'' . $release_info->{distribution} . '\'');
+
+if ( not defined $release_info->{abstract} ) {
+  $fh->say( 'DESCRIPTION=\'' . $release_info->{distribution} . '\'' );
   warn "Missing an ABSTRACT";
-} else {
-  $fh->say('DESCRIPTION=\'' .  $release_info->{abstract} . '\'');
+}
+else {
+  $fh->say( 'DESCRIPTION=\'' . $release_info->{abstract} . '\'' );
 }
 
-my $lics = [];
+my $lics   = [];
 my $licmap = {
-  perl_5 => [qw( Artistic GPL-2 )],
+  perl_5     => [qw( Artistic GPL-2 )],
   apache_2_0 => [qw( Apache-2.0 )],
-  mit => [qw( MIT )],
-  lgpl_2_1 => [qw( LGPL-2.1 )]
+  mit        => [qw( MIT )],
+  lgpl_2_1   => [qw( LGPL-2.1 )]
 };
 
-for my $lic ( @{ $release_info->{license} } ){ 
-  if ( exists $licmap->{$lic} ){ 
-    push @$lics, @{ $licmap->{$lic}};
-  } else {
+for my $lic ( @{ $release_info->{license} } ) {
+  if ( exists $licmap->{$lic} ) {
+    push @$lics, @{ $licmap->{$lic} };
+  }
+  else {
     warn "No Gentoo maping listed for $lic license type";
   }
 }
 
-if( scalar @$lics == 1 ){ 
-  $fh->say('LICENSE=" ' . $lics->[0] . '"');
-} elsif ( scalar @$lics > 1 ){
-  $fh->say('LICENSE=" || ( ' . (join q{ } , @$lics) . ' )"');
-} else {
+if ( scalar @$lics == 1 ) {
+  $fh->say( 'LICENSE=" ' . $lics->[0] . '"' );
+}
+elsif ( scalar @$lics > 1 ) {
+  $fh->say( 'LICENSE=" || ( ' . ( join q{ }, @$lics ) . ' )"' );
+}
+else {
   $fh->say('LICENSE=""');
 }
 $fh->say('SLOT="0"');
 $fh->say('KEYWORDS="~amd64 ~x86"');
-if( $handler2->has_tdeps ) { 
+if ( $handler2->has_tdeps ) {
   $fh->say('IUSE="test"');
-} else {
+}
+else {
   $fh->say('IUSE=""');
 }
 
 if ( $handler2->has_cdeps ) {
   $fh->say('perl_meta_configure() {');
   for my $dep ( @{ $handler2->cdeps } ) {
-    $fh->say("\t# " . $dep->{dep});
-    $fh->say("\techo " . $dep->{install});
+    $fh->say( "\t# " . $dep->{dep} );
+    $fh->say( "\techo " . $dep->{install} );
   }
   $fh->say('}');
-  push @{ $depends }, '$(perl_meta_configure)';
+  push @{$depends}, '$(perl_meta_configure)';
 }
 if ( $handler2->has_bdeps ) {
   $fh->say('perl_meta_build() {');
   for my $dep ( @{ $handler2->bdeps } ) {
-    $fh->say("\t# " . $dep->{dep});
-    $fh->say("\techo " . $dep->{install});
+    $fh->say( "\t# " . $dep->{dep} );
+    $fh->say( "\techo " . $dep->{install} );
   }
   $fh->say('}');
-  push @{ $depends }, '$(perl_meta_build)';
+  push @{$depends}, '$(perl_meta_build)';
 
 }
 if ( $handler2->has_rdeps ) {
   $fh->say('perl_meta_runtime() {');
   for my $dep ( @{ $handler2->rdeps } ) {
-    $fh->say("\t# " . $dep->{dep});
-    $fh->say("\techo " . $dep->{install});
+    $fh->say( "\t# " . $dep->{dep} );
+    $fh->say( "\techo " . $dep->{install} );
   }
   $fh->say('}');
-  push @{ $depends }, '$(perl_meta_runtime)';
-  push @{ $rdepends }, '$(perl_meta_runtime)';
+  push @{$depends},  '$(perl_meta_runtime)';
+  push @{$rdepends}, '$(perl_meta_runtime)';
 
 }
 if ( $handler2->has_tdeps ) {
   $fh->say('perl_meta_test() {');
   for my $dep ( @{ $handler2->tdeps } ) {
-    $fh->say("\t# " . $dep->{dep});
-    $fh->say("\techo " . $dep->{install});
+    $fh->say( "\t# " . $dep->{dep} );
+    $fh->say( "\techo " . $dep->{install} );
   }
   $fh->say('}');
-  push @{ $depends }, 'test? ( $(perl_meta_test) )';
+  push @{$depends}, 'test? ( $(perl_meta_test) )';
 }
 
-$fh->say("DEPEND=\"\n" .  ( join qq{\n}, map { "\t$_" } @{$depends} ) . "\n\"");
-$fh->say("RDEPEND=\"\n" .  ( join qq{\n}, map { "\t$_" } @{$rdepends} ) . "\n\"");
+$fh->say( "DEPEND=\"\n" .  ( join qq{\n}, map { "\t$_" } @{$depends} ) . "\n\"" );
+$fh->say( "RDEPEND=\"\n" . ( join qq{\n}, map { "\t$_" } @{$rdepends} ) . "\n\"" );
 $fh->say("SRC_TEST=\"do\"");
 
 #say pp( \%modules,);# { pretty => 1 } );
