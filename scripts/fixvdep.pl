cf352a74150e28c06f15b801e9edf239ffcc4be7
Kent Fredric <kentfredric@gmail.com>
2012-03-06 03:52:30 +1300

    [scripts/fixvdep.pl] Clarify usage in documentation

--- scripts/fixvdep.pl
+++ scripts/fixvdep.pl
@@ -6,7 +6,20 @@ use warnings;
 # CREATED: 12/11/11 00:52:15 by Kent Fredric (kentnl) <kentfredric@gmail.com>
 # ABSTRACT: Fix up version deps on a specific package in (a) given ebuild(s)
 
+if ( not @ARGV or grep { /-h|--help/ } @ARGV ) {
+    print <<"MSG";
 
+    Usage:
+
+    fixvdep.pl Perl-Module-Name oldformat newformat \@files;
+
+    ie:
+
+    fixdep.pl  Moose 0.91 0.910.0 ./*/*.ebuild
+
+MSG
+    exit 0;
+}
 my ( $pkg, $oldversion , $newversion , @files ) = @ARGV;
 @ARGV = ();
 
